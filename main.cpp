64d67632eb3831baf561c772f2f526b7ae6019b8
Carlos Eduardo Gesser <gesser@gmail.com>
2017-04-07 12:40:25 +0000

    More optimizations

--- main.cpp
+++ main.cpp
@@ -5,17 +5,6 @@ struct Transition
     S next;
     P predicate;
     A action;
-    
-    template<typename Input>
-    bool operator()(Input input) 
-    {
-        return predicate(input);
-    }
-    
-    void operator()() 
-    {
-        action();
-    }
 };
 
 template<typename S, typename P, typename A>
@@ -25,53 +14,48 @@ Transition<S,P,A> make_transition(S state, S next,  P predicate, A action)
 }
 
 template<typename S, typename I, typename T, typename ...Tn>
-S next(S state, I input, S err, T t1, Tn ...tn)
+inline S next(S state, I input, S err, T t1, Tn ...tn)
 {
-    if (t1.state == state && t1(input))
+    if (t1.state == state && t1.predicate(input))
     {
-        t1();
+        t1.action();
         return t1.next;
     }
     return next(state, input, err, tn...);
 }
 
-template<typename S, typename I, typename T>
-S next(S state, I input, S err, T t1)
+template<typename S, typename I>
+inline S next(S state, I input, S err)
 {
-    if (t1.state == state && t1(input))
-    {
-        t1();
-        return t1.next;
-    }
     return err;
 }
 
-template<typename Iter, typename State, typename ...Transitions>
-State run(Iter begin, Iter end, State start, State error, Transitions ...transitions )
+template<typename Iter, typename State, typename Validation, typename ...Transitions>
+State run(Iter begin, Iter end, State start, State error, Validation validation, Transitions ...transitions )
 {
-    while (begin != end && start != error)
+    while ( validation(start) && begin != end)
     {
-        start = next(start, *begin++, error, transitions...);
+        auto c = *begin++;
+        start = next(start, c, error, transitions...);
     }
     
     return start;
 }
 
+//--------------------------------------------------------------------------------------------------------------------
+
 #include <iostream>
 
-bool test(const char *begin, const char *end)
+auto test(const char *begin, const char *end)
 {
     enum State { A, B, E };
     
-    return run(begin, end, A, E,
+    return run(begin, end, A, E, [](State s) { return s != E; },
                  make_transition(A, B, [](char c){ return c == 'a'; }, [](){ std::cout << "A->B" << std::endl; }),
                  make_transition(B, B, [](char c){ return c == 'b'; }, [](){ std::cout << "B->B" << std::endl; }),
                  make_transition(B, A, [](char c){ return c != 'b'; }, [](){ std::cout << "B->A" << std::endl; })
                 );
 }
-
-
-
 
 int main(int argc, char * argv[])
 {
