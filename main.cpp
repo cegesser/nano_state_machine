64447d3d08be581990f48d28972eb21f9f94be6a
Carlos Eduardo Gesser <gesser@gmail.com>
2017-04-07 15:25:35 +0000

    Http status line test

--- main.cpp
+++ main.cpp
@@ -13,6 +13,18 @@ Transition<S,P,A> make_transition(S state, S next,  P predicate, A action)
     return { state, next, predicate, action };
 }
 
+struct Noop
+{
+    template<typename I>
+    void operator()(I) const {  } 
+};
+
+template<typename S, typename P>
+Transition<S,P,Noop> make_transition(S state, S next,  P predicate)
+{
+    return { state, next, predicate, Noop() };
+}
+
 template<typename S, typename I, typename T, typename ...Tn>
 inline S next(S state, I input, S err, T t1, Tn ...tn)
 {
@@ -45,41 +57,40 @@ State run(Iter begin, Iter end, State start, State error, Validation validation,
 
 //--------------------------------------------------------------------------------------------------------------------
 
-auto test(const char *begin, const char *end)
-{
-    enum State { A, B, E };
-    
-    const char *a_start, *a_end; 
-
-     run(begin, end, A, E, [](State s) { return s != E; },
-                 make_transition(A, B, [](char c){ return c == 'a'; }, [&](const char *i){ a_start=i; }),
-                 make_transition(B, B, [](char c){ return c == 'b'; }, [](const char *i){ }),
-                 make_transition(B, A, [](char c){ return c != 'b'; }, [&](const char *i){ a_end=i; })
-                );
-    return a_end - a_start;
-}
-
 
 //--------------------------------------------------------------------------------------------------------------------
 
 #include <iostream>
+#include <string>
 
-auto test2(const char *begin, const char *end)
+bool simple_http_request_line_test(const char *begin, const char *end)
 {
-    enum State { A, B, E };
+    enum State { Method, FirstSpace, RequestTarget, SecondSpace, HttpVersion, CR, LF, Error };
     
-    return run(begin, end, A, E, [](State s) { return s != E; },
-                 make_transition(A, B, [](char c){ return c == 'a'; }, [](const char *){ std::cout << "A->B" << std::endl; }),
-                 make_transition(B, B, [](char c){ return c == 'b'; }, [](const char *){ std::cout << "B->B" << std::endl; }),
-                 make_transition(B, A, [](char c){ return c != 'b'; }, [](const char *){ std::cout << "B->A" << std::endl; })
-                );
+    const char *start_method = begin;
+    const char *start_request_target;
+    const char *start_http_version;
+    
+    auto result = run(begin, end, Method, Error, [](State s) { return s != Error; },
+        make_transition(Method,        Method,        [](char c){ return c != ' '; } ),
+        make_transition(Method,        FirstSpace,    [](char c){ return c == ' '; }, [&](const char *i){ std::cout << "Method: " << std::string(start_method, i) << std::endl; }  ),
+        make_transition(FirstSpace,    RequestTarget, [](char c){ return c != ' '; }, [&](const char *i){ start_request_target = i; }  ),
+        make_transition(RequestTarget, RequestTarget, [](char c){ return c != ' '; } ),
+        make_transition(RequestTarget, SecondSpace,   [](char c){ return c == ' '; }, [&](const char *i){ std::cout << "Request Target: " << std::string(start_request_target, i) << std::endl; }  ),
+        make_transition(SecondSpace,   HttpVersion,   [](char c){ return c != ' '; }, [&](const char *i){ start_http_version = i; }  ),
+        make_transition(HttpVersion,   HttpVersion,   [](char c){ return c != '\r'; } ),
+        make_transition(HttpVersion,   CR,            [](char c){ return c == '\r'; }, [&](const char *i){ std::cout << "HTTP Version: " << std::string(start_http_version, i) << std::endl; } ),
+        make_transition(CR,            LF,            [](char c){ return c == '\n'; } )
+    );
+    std::cout << "Last State: " << result << std::endl;
+    return result == LF;
 }
 
 int main(int argc, char * argv[])
 {
     
-    char txt[] = "abbc";
-    std::cout << test(txt, txt+sizeof(txt)-1) << std::endl;
+    char txt[] = "GET /index.html HTTP/1.1\r\n";
+    std::cout << std::boolalpha << simple_http_request_line_test(txt, txt+sizeof(txt)-1) << std::endl;
     return 0;
     
 }
